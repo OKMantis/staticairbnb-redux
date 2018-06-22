bb10216516ec13e29f3e540e24a58e1d3410aa13
Owen Moss <owenkmoss@gmail.com>
2018-06-22 19:31:14 +0200

    added inline styling on map container

--- src/containers/map.jsx
+++ src/containers/map.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Map extends Component {
   render() {
     return(
-      <div className="col-sm-5">
+      <div className="col-sm-5" style={{height: '100vh'}} >
         TO DO
       </div>
     );
