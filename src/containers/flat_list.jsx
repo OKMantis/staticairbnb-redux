df31f7fadaa6ada56afe0b939c86aa96ef3b0abe
Owen Moss <owenkmoss@gmail.com>
2018-06-22 19:33:29 +0200

    added styling css to flatlist container

--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class FlatList extends Component {
   render() {
     return(
-      <div className="col-sm-7">
+      <div className="flat-list col-sm-7">
         TO DO
       </div>
     );
