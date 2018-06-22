37a84700947d96e423c2132ec5ccdb13b2bbf78c
Owen Moss <owenkmoss@gmail.com>
2018-06-22 20:01:27 +0200

    added temporary props of one flat and calling flat component inside flatlist div to show flat inside flatlist

--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
+import Flat from '../components/flat';
+
 class FlatList extends Component {
+// TEMPORARY CODE TO INTEGRATE HTML
+  static defaultProps = {
+    flats: [{
+     "name": "Charm at the Steps of Montmartre",
+     "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+     "price": 164,
+     "priceCurrency": "EUR"
+    }]
+  }
+
   render() {
     return(
       <div className="flat-list col-sm-7">
-        TO DO
+        {this.props.flats.map((flat) => <Flat flat={flat} key={flat.name} />)}
       </div>
-    );
+      );
   }
 }
 
