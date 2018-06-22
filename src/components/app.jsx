3357016a0d5d402c4feced61483572f2baf6425b
Owen Moss <owenkmoss@gmail.com>
2018-06-22 19:03:49 +0200

    initial creation app

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    <div>
+      <FlatList />
+      <Map />
+    </div>
+  }
+}
+
+// FlatList and Map are both containers which
+// need to be created inside the containers
+// folder.
+
+export default App;
