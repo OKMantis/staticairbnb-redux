3f0a08638458059ab73a7ab77bc92c7a882b9483
Owen Moss <owenkmoss@gmail.com>
2018-06-22 19:05:30 +0200

    modified index.jsx to render the app

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = props => <div>Hello, {props.name}</div>;
+import App from './components/app';
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<App />, root);
 }
