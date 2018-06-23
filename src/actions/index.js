fb77b58514c399c8e51e1662bd732f3d28dea6aa
Owen Moss <owenkmoss@gmail.com>
2018-06-23 16:48:15 +0200

    added explanation to code

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,12 @@ export function setFlats() {
 
  return {
    type: 'SET_FLATS',
-   payload: flats
+   payload: flats // this is inside flats.js
  }
 }
+
+// 2. Function setFlats is triggered by componentWillMount function
+  // inside the container flat_list.jsx (line 14).
+  // setFlats does an API call and returns the action
+  // SET_FLATS with the payload flats.
+  // Next thing happens inside flat_reducer...
