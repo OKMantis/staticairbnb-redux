c6503aa9cf4113945686e0bf6d5db47c4bb551a3
Owen Moss <owenkmoss@gmail.com>
2018-06-23 16:46:13 +0200

    setting up mapDispatchToProps(),  componentWillMount() and mapStateToProps()

--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -1,31 +1,57 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+// Above is needed to make mapDispatchToProps function work
+import { connect } from 'react-redux';
+// Is to connect mapDispatchToProps function to the component
+import { setFlats } from '../actions';
+// Above is importing the setFlats action so it can be
+// used by mapDispatchToProps function.
 
 import Flat from '../components/flat';
 
 class FlatList extends Component {
-// TEMPORARY CODE TO INTEGRATE HTML
-  static defaultProps = {
-    flats: [{
-     "name": "Charm at the Steps of Montmartre",
-     "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-     "price": 164,
-     "priceCurrency": "EUR"
-    }]
-  }
 
   componentWillMount() {
-    // TODO: dispatch an action to update the Redux state
-    // tree (flats)
+    this.props.setFlats(); // 1. Triggers action setFlats (see ../actions/index.js)
   }
+  // Above when the component mounts. I will call
+  // this.props.setFlats() and it will trigger the
+  // action in Redux. The action will fetch the flats
+  // from the API. Then the reducer will receive this
+  // action and update the Redux store.
 
   render() {
     return(
-      <div className="flat-list col-sm-7">
+      <div className="flat_list col-sm-7">
         {this.props.flats.map((flat) => <Flat flat={flat} key={flat.name} />)}
       </div>
       );
   }
 }
 
+function mapDispatchToProps(dispatch) {
+ return bindActionCreators(
+ { setFlats: setFlats },
+ dispatch
+ );
+}
+// Above this function is made to wire the action setFlats
+// to the props of the flat-list component.
 
-export default FlatList;
+function mapStateToProps(state) {
+ return {
+ flats: state.flats
+ };
+}
+// Above we're mapping the Redux state from the external
+// store to actual props in the component.
+
+// 4. Thanks to the wiring of the Redux state to props
+  //  we say that ok the state.flats can go directly into
+  // React props and this way we have now access to
+  // this.props.flats (see line 26).
+
+export default connect(mapStateToProps, mapDispatchToProps)
+(FlatList);
+// This will connect the component with mapDispatchToProps
+// and mapStateToProps.
