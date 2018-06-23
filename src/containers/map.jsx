75b8f3aee0dbe51c99d304e98f93919c46f55df5
Owen Moss <owenkmoss@gmail.com>
2018-06-23 18:35:29 +0200

    connected mapStateToProps to Map component, if statement for when flat is elected with the marker showing on the map

--- src/containers/map.jsx
+++ src/containers/map.jsx
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { connect } from 'react-redux';
 
 class Map extends Component {
-  render() {
-    return(
-      <div className="col-sm-5" style={{height: '100vh'}} >
-        TO DO
-      </div>
+ render() {
+   let marker = null;
+   let center = { lat: 48.856614, lng: 2.352222 };
+
+    if (this.props.selectedFlat) {
+     marker = <div
+     style={{ width: '20px', height: '20px',
+     backgroundColor: 'red',
+     borderRadius: '50%' }}
+     lat={this.props.selectedFlat.lat}
+     lng={this.props.selectedFlat.lng} />;
+     center = { lat: this.props.selectedFlat.lat,
+       lng: this.props.selectedFlat.lng };
+    }
+
+    // Above if the selectedFLat in the props which has
+    // been wired through mapStateToProps (line 36)
+    // and which has been connected to the map (line 41).
+    // is not null then the marker will be the div you
+    // see above with the information obtained from the
+    // selectedFlat props (lat and lng)
+
+   return (
+     <div className="col-sm-5" style={{height: '100vh'}}>
+     <GoogleMapReact
+     center={center}
+     defaultZoom={15}>
+     {marker}
+     </GoogleMapReact>
+     </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+return { selectedFlat: state.selectedFlat };
+}
 
-export default Map;
+export default connect(mapStateToProps)(Map);;
